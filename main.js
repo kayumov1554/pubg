8ad26a3b885f73d80f1f47b594f04fff57e7893b
kayumov1554 <167546948+kayumov1554@users.noreply.github.com>
2024-07-28 01:19:38 +0500

    ssss

--- main.js
+++ main.js
@@ -41,36 +41,58 @@ bot.onText(/\/start|\/help/, (msg) => {
     },
   };
 
-  bot.sendMessage(msg.chat.id, welcomeMessage, options);
-});
+  bot.sendMessage(msg.chat.id, welcomeMessage, options).then((sentMessage) => {
+    // Save the message ID to edit it later
+    bot.on("callback_query", (callbackQuery) => {
+      const msg = callbackQuery.message;
+      const data = callbackQuery.data;
+      const chatId = msg.chat.id;
+      const messageId = sentMessage.message_id;
 
-bot.on("callback_query", (callbackQuery) => {
-  const msg = callbackQuery.message;
-  const data = callbackQuery.data;
+      if (data === "uc") {
+        const ucPrices = [
+          { uc: "60-UC", price: "12.000 so'm" },
+          { uc: "120-UC", price: "25.000 so'm" },
+          { uc: "180-UC", price: "38.000 so'm" },
+          { uc: "350-UC", price: "55.000 so'm" },
+          { uc: "600-UC", price: "100.000 so'm" },
+          { uc: "900-UC", price: "150.000 so'm" },
+          { uc: "1240-UC", price: "270.000 so'm" },
+          { uc: "1870-UC", price: "350.000 so'm" },
+        ];
 
-  if (data === "uc") {
-    const ucPrices = [
-      { uc: "60-UC", price: "12.000 so'm" },
-      { uc: "120-UC", price: "25.000 so'm" },
-      { uc: "180-UC", price: "38.000 so'm" },
-      { uc: "350-UC", price: "55.000 so'm" },
-      { uc: "600-UC", price: "100.000 so'm" },
-      { uc: "900-UC", price: "150.000 so'm" },
-      { uc: "1240-UC", price: "270.000 so'm" },
-      { uc: "1870-UC", price: "350.000 so'm" },
-    ];
+        const message =
+          "<b>MUSAA_PUBG_UC narxlari</b>\n\n" +
+          ucPrices
+            .map(
+              (price) =>
+                `<pre>${price.uc.padEnd(10)}: ${price.price.padStart(10)}</pre>`
+            )
+            .join("\n");
 
-    const message =
-      "<b>MUSAA_PUBG_UC narxlari</b>\n\n" +
-      ucPrices
-        .map(
-          (price) =>
-            `<pre>${price.uc.padEnd(10)}: ${price.price.padStart(10)}</pre>`
-        )
-        .join("\n");
-
-    bot.sendMessage(msg.chat.id, message, { parse_mode: "HTML" });
-  }
+        bot
+          .editMessageText(message, {
+            chat_id: chatId,
+            message_id: messageId,
+            parse_mode: "HTML",
+            reply_markup: {
+              inline_keyboard: [
+                [{ text: "Sotib olish", url: "https://t.me/kayumov_1554" }],
+              ],
+            },
+          })
+          .then(() => {
+            const cardInfo = `
+              <b>CARD INFO</b>
+              <pre>CARD-4046320002166858</pre>
+              <pre>Ism-MuhammadSodiq</pre>
+              <pre>Familiya-Kayumov</pre>
+            `;
+            bot.sendMessage(chatId, cardInfo, { parse_mode: "HTML" });
+          });
+      }
+    });
+  });
 });
 
 app.listen(port, () => {
